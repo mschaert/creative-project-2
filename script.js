9112034d79a963a8413af33179751a33f530c808
Mickey Schaertl <mschaert@byu.edu>
2018-10-01 16:41:50 +0000

    initialize repo

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#randomFactButton").click(function(e) {
+    var url = "https://api.chucknorris.io/jokes/random";
+
+    $.ajax({
+        url: url,
+        dataType: "json",
+        success: function(parsed_json) {
+            console.log(parsed_json.value);
+            $("#randomFact").html(parsed_json.value);
+        }
+    });
+});
