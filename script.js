7be9fc206723cdbcf4fa1dcbb3a92db24c4fa29b
EC2 Default User <ec2-user@ip-172-31-35-204.us-west-2.compute.internal>
2018-10-04 02:06:25 +0000

    Added styling and R functionality

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-$("#randomFactButton").click(function(e) {
-    var url = "https://api.chucknorris.io/jokes/random";
+var url = "https://api.chucknorris.io/jokes/random";
+var cat_add_on = "?category=";
+var current_category = "all";
+var is_cat_active = false;
 
-    $.ajax({
-        url: url,
+$(document).ready(function() {
+    getFact();
+    
+    $(document).keypress(function(e) {
+            if ( e.keyCode === 82 || e.keyCode === 114 ) // R,r
+                $("#randomFact").empty();
+                getFact();
+        });
+        
+    $('#categorySelect').on('change',() => {
+        updateCategory()
+    })
+})
+
+function getFact() {
+    var cat_url
+    if(is_cat_active) {
+        cat_url = url + cat_add_on + current_category;
+    } 
+    else {
+        cat_url = url
+    }
+
+    
+    $('.loader').fadeIn(500, function() {
+            $.ajax({
+        url: cat_url,
         dataType: "json",
         success: function(parsed_json) {
             console.log(parsed_json);
-            $("#randomFact").html(parsed_json.value);
+            
+            if(parsed_json.category == "explicit") {
+                getFact();
+                return;
+            }
+            printFact(parsed_json.value);
         }
     });
-});
+    })
+
+}
+
+function printFact(factHTML) {
+    $('.loader').fadeOut(500, function() {
+             $("#randomFact").html(factHTML);
+    })
+}
+
+function updateCategory() {
+    var select = $('#categorySelect');
+    if(select.val() == "all") {
+        is_cat_active = false;
+    } else {
+        is_cat_active = true;
+    }
+    current_category = select.val();
+    $('#category').html(current_category);
+}
