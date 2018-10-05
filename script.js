506e2b7277a2ba8f65ef8e8ed6587a96104cae72
Mickey Schaertl <mschaert@byu.edu>
2018-10-05 04:59:24 +0000

    Fix R refresh, add icon click

--- script.js
+++ script.js
@@ -4,60 +4,63 @@ var current_category = "all";
 var is_cat_active = false;
 
 $(document).ready(function() {
-    getFact();
-    
-    $(document).keypress(function(e) {
-            if ( e.keyCode === 82 || e.keyCode === 114 ) // R,r
-                $("#randomFact").empty();
-                getFact();
-        });
-        
-    $('#categorySelect').on('change',() => {
-        updateCategory()
-    })
-})
+	getFact();
+
+	$(document).keypress(function(e) {
+		if (e.keyCode === 82 || e.keyCode === 114) { // R,r
+			$("#randomFact").empty();
+			getFact();
+		}
+	});
+
+	$("#chuckIcon").click(function() {
+		$("#randomFact").empty();
+		getFact();
+	});
+
+	$('#categorySelect').change(function() {
+		updateCategory();
+	});
+});
 
 function getFact() {
-    var cat_url
-    if(is_cat_active) {
-        cat_url = url + cat_add_on + current_category;
-    } 
-    else {
-        cat_url = url
-    }
+	var cat_url;
+	if (is_cat_active) {
+		cat_url = url + cat_add_on + current_category;
+	} else {
+		cat_url = url;
+	}
 
-    
-    $('.loader').fadeIn(500, function() {
-            $.ajax({
-        url: cat_url,
-        dataType: "json",
-        success: function(parsed_json) {
-            console.log(parsed_json);
-            
-            if(parsed_json.category == "explicit") {
-                getFact();
-                return;
-            }
-            printFact(parsed_json.value);
-        }
-    });
-    })
+	$('.loader').fadeIn(500, function() {
+		$.ajax({
+			url: cat_url,
+			dataType: "json",
+			success: function(parsed_json) {
+				console.log(parsed_json);
 
+				if (parsed_json.category == "explicit") {
+					getFact();
+					return;
+				}
+				printFact(parsed_json.value);
+			}
+		});
+	});
 }
 
 function printFact(factHTML) {
-    $('.loader').fadeOut(500, function() {
-             $("#randomFact").html(factHTML);
-    })
+	$('.loader').fadeOut(500, function() {
+		$("#randomFact").html(factHTML);
+	});
 }
 
 function updateCategory() {
-    var select = $('#categorySelect');
-    if(select.val() == "all") {
-        is_cat_active = false;
-    } else {
-        is_cat_active = true;
-    }
-    current_category = select.val();
-    $('#category').html(current_category);
+	var select = $('#categorySelect');
+	if (select.val() == "all") {
+		is_cat_active = false;
+	} else {
+		is_cat_active = true;
+	}
+	current_category = select.val();
+	$('#category').html(current_category);
 }
